707eb8a08d2be7fb5af8c5652d395e1bdf230268
VebriAriadi2 <vebriariadi2@gmail.com>
2023-11-24 20:37:38 +0700

    Update dash.php

--- database/dash.php
+++ database/dash.php
@@ -1,5 +1,5 @@
 <?php
-// getNama.php
+// getProfile.php
 
 // Koneksi ke database
 include 'koneksi.php';
@@ -7,25 +7,22 @@ include 'koneksi.php';
 // Ambil NIP/NISN dari permintaan POST
 $nipnisn = $_POST['nipnisn'];
 
-// Lakukan kueri ke database untuk mendapatkan nama dari tabel siswa
-$querySiswa = $querySiswa = "SELECT siswa.nama, siswa.nisn, siswa.email, siswa.no_hp, siswa.alamat, siswa.role, siswa.kelas_id, siswa.foto, kelas.nama_kelas
-                             FROM siswa JOIN kelas ON siswa.kelas_id = kelas.id_kelas WHERE siswa.nisn = '$nipnisn'";
+// Lakukan kueri ke database untuk mendapatkan data siswa termasuk foto (tipe blob)
+$querySiswa = "SELECT siswa.nama, siswa.nisn, siswa.email, siswa.no_hp, siswa.alamat, siswa.role, siswa.kelas_id, siswa.foto, kelas.nama_kelas
+               FROM siswa JOIN kelas ON siswa.kelas_id = kelas.id_kelas WHERE siswa.nisn = '$nipnisn'";
 
 $resultSiswa = mysqli_query($db, $querySiswa);
-
-
 
 // Gabungkan hasil query menggunakan UNION
 $result = array();
 if ($resultSiswa) {
     while ($row = mysqli_fetch_assoc($resultSiswa)) {
+        // Konversi data blob menjadi base64 untuk disertakan dalam respons JSON
+        $row['foto'] = base64_encode($row['foto']);
         $result[] = $row;
     }
 }
 
-
-// Kirim nama sebagai respons JSON
+// Kirim data siswa sebagai respons JSON
 echo json_encode(['profile' => $result]);
-
-
 ?>
