d10b3392343ba7b5b8d2c605342bc2b510c76164
Wahyu2003 <124946688+Wahyu2003@users.noreply.github.com>
2023-11-24 09:12:43 +0700

    update design

--- admin/home.php
+++ admin/home.php
@@ -4,17 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SIM PASDATA | Admin Home</title>
+    <link rel="stylesheet" href="../main/style.css">
 </head>
 <body>
     <?php
         include "../main/menu.php";
     ?>
-    <h1>Halaman Home Admin, Selamat Datang <?php 
-        $namaAdmin = $_SESSION['namaAdmin'];
-        echo $namaAdmin;
-    ?>
-    </h1>
-    
-    
+    <main>
+			<div class="head-title">
+				<div class="left">
+					<h1>Dashboard</h1>
+					<ul class="breadcrumb">
+						<li>
+							<a href="#">Dashboard</a>
+						</li>
+						<li><i class='bx bx-chevron-right' ></i></li>
+						<li>
+							<a class="active" href="#">Home</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+            <h1 class="box-info">Halaman Home Admin, Selamat Datang 
+            <?php 
+                $namaAdmin = $_SESSION['namaAdmin'];
+                echo $namaAdmin;
+            ?>
+            </h1>
+		</main> 
 </body>
 </html>
--- main/menu.php
+++ main/menu.php
@@ -1,199 +1,209 @@
 <?php  include "../main/auth.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>SideBar</title>
-    <link rel="stylesheet" href="../main/menu.css" />
-    <link
-      rel="stylesheet"
-      href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
-    />
-  </head>
-  <body>
-    <nav class="sidebar">
-      <header>
-        <i class="bx bx-chevron-right toggle"></i>
-      </header>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-      <?php
-     if ($role == 'admin') { ?>
-     <div class="text header-text">
-            <span class="main"> <?php 
-        $namaAdmin = $_SESSION['namaAdmin'];
-        echo $namaAdmin;?> || Admin</span>
-          </div>
-      <div class="menu-bar">
-        <div class="menu">
-          <ul class="menu-links">
-            <!-- <li class="search-bar">
-              <i class="bx bx-search icons"></i>
-              <input type="search" placeholder="Search..." />
-            </li> -->
-            <li class="nav-link">
-              <a href="./home.php">
-                <i class="bx bx-home-alt icons"></i>
-                <span class="text nav-text">Home</span>
-              </a>
-            </li>
-            <li class="nav-link">
-              <a href="./menu_manajemen_akun_pembina.php">
-                <i class='bx bxs-user-account icons'></i>
-                <span class="text nav-text">Manajemen Akun Pembina</span>
-              </a>
-            </li>
-            <li class="nav-link">
-              <a href="./menu_manajemen_akun_senior.php">
-                <i class='bx bxs-user-account icons'></i>
-                <span class="text nav-text">Manajemen Akun Senior</span>
-              </a>
-            </li>
-            <li class="nav-link">
-              <a href="./junior_manajemen_akun.php">
-                <i class='bx bxs-user-account icons'></i>
-                <span class="text nav-text">Manajemen Akun Junior</span>
-              </a>
-            </li>
-            <li class="nav-link">
-              <a href="./data_terhapus.php">
-                <i class='bx bx-trash icons'></i>
-                <span class="text nav-text">Data Terhapus</span>
-              </a>
-            </li>
-          </ul>
-        </div>
+	<!-- Boxicons -->
+	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+	<!-- My CSS -->
+	<link rel="stylesheet" href="../main/style.css">
 
+	<title>SideBar</title>
+</head>
+<body>
+    <?php
+        if ($role == 'admin') { ?>
+
+	<!-- SIDEBAR -->
+    <section id="sidebar">
+      <a href="#" class="brand">
+        <i class='bx bxs-smile'></i>
+          <span class="text">
+          <?php 
+                $namaAdmin = $_SESSION['namaAdmin'];
+                echo $namaAdmin;?> || Admin
+          </span>
+        
+      </a>
+      <ul class="side-menu top">
+        <li class="active">
+          <a href="./home.php">
+            <i class='bx bx-home-alt' ></i>
+            <span class="text">Home</span>
+          </a>
+        </li>
+        <li>
+          <a href="./menu_manajemen_akun_pembina.php">
+            <i class='bx bxs-user-account' ></i>
+            <span class="text">Manajemen Akun Pembina</span>
+          </a>
+        </li>
+        <li>
+          <a href="./menu_manajemen_akun_senior.php">
+            <i class='bx bxs-doughnut-chart' ></i>
+            <span class="text">Manajemen Akun Senior</span>
+          </a>
+        </li>
+        <li>
+          <a href="./junior_manajemen_akun.php">
+            <i class='bx bxs-message-dots' ></i>
+            <span class="text">Manajemen Akun Junior</span>
+          </a>
+        </li>
+        <li>
+          <a href="./data_terhapus.php">
+            <i class='bx bx-trash' ></i>
+            <span class="text">Data Terhapus</span>
+          </a>
+        </li>
+      </ul>
       <?php }
       elseif ($role == 'pembina') { ?>
-        <div class="text header-text">
-            <span class="main"> <?php 
-        $namaAdmin = $_SESSION['namaAdmin'];
-        echo $namaAdmin;?> || Pembina</span>
-          </div>
-          <div class="menu-bar">
-        <div class="menu">
-          <ul class="menu-links">
-            <!-- <li class="search-bar">
-              <i class="bx bx-search icons"></i>
-              <input type="search" placeholder="Search..." />
-            </li> -->
-            <li class="nav-link">
-              <a href="./home.php">
-                <i class="bx bx-home-alt icons"></i>
-                <span class="text nav-text">Home</span>
-              </a>
-            </li>
-            <li class="nav-link">
-              <a href="./menu_manajemen_akun_senior.php">
-                <i class='bx bxs-user-account icons'></i>
-                <span class="text nav-text">Manajemen Akun Senior</span>
-              </a>
-            </li>
-            <li class="nav-link">
-              <a href="./junior_manajemen_akun.php">
-                <i class='bx bxs-user-account icons'></i>
-                <span class="text nav-text">Manajemen Akun Junior</span>
-              </a>
-            </li>
-            <li class="nav-link">
-              <a href="./nilai_senior.php">
-                <i class='bx bx-edit icons'></i>
-                <span class="text nav-text">Nilai Senior</span>
-              </a>
-            </li>
-            <li class="nav-link">
-              <a href="./data_terhapus.php">
-                <i class='bx bx-trash icons'></i>
-                <span class="text nav-text">Data Terhapus</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-
-        <?php }
-        elseif ($role == 'senior') { ?>
-          <div class="text header-text">
-            <span class="main"> <?php 
-        $namaSiswa = $_SESSION['namaSiswa'];
-        
-        echo $namaSiswa;?> || Senior</span>
-          </div>
-          <div class="menu-bar">
-        <div class="menu">
-          <ul class="menu-links">
-            <!-- <li class="search-bar">
-              <i class="bx bx-search icons"></i>
-              <input type="search" placeholder="Search..." />
-            </li> -->
-            <li class="nav-link">
-              <a href="./home.php">
-                <i class="bx bx-home-alt icons"></i>
-                <span class="text nav-text">Home</span>
-              </a>
-            </li>
-            <li class="nav-link">
-              <a href="./junior_manajemen_akun.php">
-                <i class='bx bxs-user-account icons'></i>
-                <span class="text nav-text">Manajemen Akun Junior</span>
-              </a>
-            </li>
-            <li class="nav-link">
-              <a href="./nilai_junior.php">
-                <i class='bx bx-edit icons'></i>
-                <span class="text nav-text">Nilai Junior</span>
-              </a>
-            </li>
-            <li class="nav-link">
-              <a href="./data_terhapus.php">
-                <i class='bx bx-trash icons'></i>
-                <span class="text nav-text">Data Terhapus</span>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <?php } ?>
-        <ul class="bottom-content"> <!-- Perbaikan: Gunakan elemen ul untuk memasukkan elemen li -->
-          <li class="nav-link">
-          <?php
-            if ($role == 'admin') { ?>
-              <a href="./profil.php">
-                <i class='bx bx-user icons'></i>
-                <span class="text nav-text"> Profil</span>
-              </a>
-          <?php }
-            elseif ($role == 'pembina') { ?>
-            <a href="./profil.php">
-                <i class='bx bx-user icons'></i>
-                <span class="text nav-text"> Profil</span>
-              </a>
-          <?php }
-            elseif ($role == 'senior') { ?>
-            <a href="./profil.php">
-                <i class='bx bx-user icons'></i>
-                <span class="text nav-text"> Profil</span>
-              </a>
-            <?php } ?>
+    <!-- SIDEBAR -->
+      <section id="sidebar">
+        <a href="#" class="brand">
+          <i class='bx bxs-smile'></i>
+            <span class="text">
+            <?php 
+                  $namaAdmin = $_SESSION['namaAdmin'];
+                  echo $namaAdmin;?> || Admin
+            </span>
+          
+        </a>
+        <ul class="side-menu top">
+          <li class="active">
+            <a href="./home.php">
+              <i class='bx bx-home-alt' ></i>
+              <span class="text">Home</span>
+            </a>
           </li>
-          <li class="mode">
-            <div class="moon-sun">
-              <i class="bx bx-moon icons moon"></i>
-              <i class="bx bx-sun icons sun"></i>
-            </div>
-            <span class="mode-text text">Dark Mode</span>
-            <div class="toggle-switch">
-              <span class="switch"></span>
-            </div>
+          <li>
+            <a href="./menu_manajemen_akun_senior.php">
+              <i class='bx bxs-doughnut-chart' ></i>
+              <span class="text">Manajemen Akun Senior</span>
+            </a>
           </li>
-          <li class="nav-link">
-            <a href="../main/signout.php">
-              <i class="bx bx-log-out icons"></i>
-              <span class="text nav-text"> Log Out</span>
+          <li>
+            <a href="./junior_manajemen_akun.php">
+              <i class='bx bxs-message-dots' ></i>
+              <span class="text">Manajemen Akun Junior</span>
+            </a>
+          </li>
+          <li>
+            <a href="./nilai_senior.php">
+              <i class='bx bx-edit' ></i>
+              <span class="text nav-text">Nilai Senior</span>
+            </a>
+          </li>
+          <li>
+            <a href="./data_terhapus.php">
+              <i class='bx bx-trash' ></i>
+              <span class="text">Data Terhapus</span>
             </a>
           </li>
         </ul>
-      </div>
-    </nav>
-    <script src="../main/script.js"></script>
-  </body>
+      <?php }
+      elseif ($role == 'senior') { ?>
+      <!-- SIDEBAR -->
+        <section id="sidebar">
+          <a href="#" class="brand">
+            <i class='bx bxs-smile'></i>
+              <span class="text">
+              <?php 
+                    $namaSiswa = $_SESSION['namaSiswa'];
+                    echo $namaSiswa;?> || Senior
+              </span>
+          </a>
+          <ul class="side-menu top">
+            <li class="active">
+              <a href="./home.php">
+                <i class='bx bx-home-alt' ></i>
+                <span class="text">Home</span>
+              </a>
+            </li>
+            <li>
+              <a href="./junior_manajemen_akun.php">
+                <i class='bx bxs-message-dots' ></i>
+                <span class="text">Manajemen Akun Junior</span>
+              </a>
+            </li>
+            <li>
+              <a href="./nilai_junior.php">
+                <i class='bx bx-edit' ></i>
+                <span class="text nav-text">Nilai Senior</span>
+              </a>
+            </li>
+            <li>
+              <a href="./data_terhapus.php">
+                <i class='bx bx-trash' ></i>
+                <span class="text">Data Terhapus</span>
+              </a>
+            </li>
+          </ul>
+          <?php } ?>
+
+
+      <ul class="side-menu">
+        <li>
+          <a href="#">
+            <i class='bx bxs-cog' ></i>
+            <span class="text">Settings</span>
+          </a>
+        </li>
+        <li>
+        <a href="../main/signout.php" class="logout">
+            <i class='bx bxs-log-out-circle' ></i>
+            <span class="text">Logout</span>
+          </a>
+        </li>
+      </ul>
+    </section>
+	<!-- SIDEBAR -->
+  
+
+	<!-- CONTENT -->
+	<section id="content">
+		<!-- NAVBAR -->
+		<nav>
+			<i class='bx bx-menu' ></i>
+			<a href="#" class="nav-link">Categories</a>
+			<form action="#">
+				<div class="form-input">
+					<input type="search" placeholder="Search...">
+					<button type="submit" class="search-btn"><i class='bx bx-search' ></i></button>
+				</div>
+			</form>
+			<input type="checkbox" id="switch-mode" hidden>
+			<label for="switch-mode" class="switch-mode"></label>
+			<a href="#" class="notification">
+				<i class='bx bxs-bell' ></i>
+				<span class="num">8</span>
+			</a>
+      <?php
+        if ($role == 'admin') { ?>
+          <a href="./profil.php" class="profile">
+            <i class='bx bx-user icons'></i>
+          </a>
+      <?php }
+        elseif ($role == 'pembina') { ?>
+        <a href="./profil.php" class="profile">
+            <i class='bx bx-user icons'></i>
+          </a>
+      <?php }
+        elseif ($role == 'senior') { ?>
+        <a href="./profil.php" class="profile">
+            <i class='bx bx-user icons'></i>
+          </a>
+      <?php } ?>
+			<!-- <a href="#" class="profile">
+				<img src="img/people.png">
+			</a> -->
+		</nav>
+		<!-- NAVBAR -->
+		<!-- MAIN -->
+	</section>
+	<script src="../main/script.js"></script>
+</body>
 </html>
