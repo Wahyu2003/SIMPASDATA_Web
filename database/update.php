c02c428b1f51c712c4b447879146cead1608d01f
VebriAriadi2 <vebriariadi2@gmail.com>
2023-11-27 02:10:50 +0700

    Update update.php

--- database/update.php
+++ database/update.php
@@ -1,65 +1,73 @@
 <?php
 require_once("koneksi.php");
 
-if (isset($_POST['update'])) {
-    $nisn = $_POST['nisn'];
-    $alamat = $_POST['alamat'];
-    $email = $_POST['email'];
-    $no_hp = $_POST['no_hp'];
-    $password = $_POST['password'];
+// Periksa apakah ada data yang dikirimkan
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Pastikan ada nilai NISN yang dikirimkan
+    if (isset($_POST['nisn'])) {
+        $nisn = $_POST['nisn'];
+        $alamat = isset($_POST['alamat']) ? $_POST['alamat'] : null;
+        $email = isset($_POST['email']) ? $_POST['email'] : null;
+        $no_hp = isset($_POST['no_hp']) ? $_POST['no_hp'] : null;
 
-    // Proses foto
-    $gambarBlob = null;
+        // Proses foto
+        $gambarBlob = null;
 
-    if (isset($_FILES['foto']['tmp_name']) && !empty($_FILES['foto']['tmp_name'])) {
-        echo json_encode(array('debug_info' => 'File received'));
-
-        $tmpName = $_FILES['foto']['tmp_name'];
-        $gambarBlob = file_get_contents($tmpName);
-        var_dump($gambarBlob);  // Add this line for debugging
-    }
-
-    // Cek apakah data siswa dengan NISN tersebut sudah ada
-    $result = mysqli_query($db, "SELECT * FROM siswa WHERE nisn = '$nisn'");
-    $cek = mysqli_num_rows($result);
-
-    if ($cek > 0) {
-        // Jika gambarBlob tidak null, masukkan ke dalam query
-        $gambarQueryPart = isset($gambarBlob) ? "`foto` = ?, " : "";
-
-        $query = "UPDATE `siswa`
-        SET `alamat` = ?, 
-            `email` = ?, 
-            `no_hp` = ?,
-            $gambarQueryPart
-            `password_siswa` = ?
-        WHERE `nisn` = ?";
-
-
-        $stmt = mysqli_prepare($db, $query);
-
-        // Urutan parameter harus sesuai dengan urutan di dalam query
-        if ($gambarBlob) {
-            // Jumlah 's' sesuaikan dengan jumlah parameter yang diikat
-            mysqli_stmt_bind_param($stmt, 'ssssss', $alamat, $email, $no_hp, $gambarBlob, $password, $nisn);
-
-        } else {
-            mysqli_stmt_bind_param($stmt, 'ssss', $alamat, $email, $no_hp, $password, $nisn);
+        // Periksa apakah file gambar diunggah dengan benar
+        if (isset($_FILES['foto']['tmp_name']) && is_uploaded_file($_FILES['foto']['tmp_name'])) {
+            $tmpName = $_FILES['foto']['tmp_name'];
+            $gambarBlob = file_get_contents($tmpName);
         }
 
-        // Eksekusi statement
-        if (mysqli_stmt_execute($stmt)) {
+        // Log sebelum eksekusi query
+        error_log("nisn: $nisn, alamat: $alamat, email: $email, no_hp: $no_hp, gambarBlob: " . strlen($gambarBlob));
+
+        // Query Update
+        $queryUpdate = "UPDATE `siswa` SET ";
+        $params = array();
+        if (!is_null($alamat)) {
+            $queryUpdate .= "`alamat` = ?, ";
+            $params[] = &$alamat;
+        }
+        if (!is_null($email)) {
+            $queryUpdate .= "`email` = ?, ";
+            $params[] = &$email;
+        }
+        if (!is_null($no_hp)) {
+            $queryUpdate .= "`no_hp` = ?, ";
+            $params[] = &$no_hp;
+        }
+        if (!is_null($gambarBlob)) {
+            $queryUpdate .= "`foto` = ?, ";
+            $params[] = &$gambarBlob;
+        }
+
+        // Hapus koma terakhir
+        $queryUpdate = rtrim($queryUpdate, ', ');
+
+        $queryUpdate .= " WHERE `nisn` = ?";
+        $stmtUpdate = mysqli_prepare($db, $queryUpdate);
+
+        // Binding parameter
+        $bindTypes = str_repeat('s', count($params)) . 's'; // String, String, Blob, String
+        call_user_func_array('mysqli_stmt_bind_param', array_merge(array($stmtUpdate, $bindTypes), $params, array(&$nisn)));
+
+        // Eksekusi statement update
+        if (mysqli_stmt_execute($stmtUpdate)) {
             echo json_encode(array('message' => 'Data berhasil diperbarui.'));
         } else {
-            echo json_encode(array('error' => 'Error: ' . mysqli_stmt_error($stmt)));
+            echo json_encode(array('error' => 'Error: ' . mysqli_stmt_error($stmtUpdate)));
         }
 
-        // Tutup statement
-        mysqli_stmt_close($stmt);
+        // Tutup statement update
+        mysqli_stmt_close($stmtUpdate);
     } else {
-        // Data siswa dengan NISN tersebut tidak ditemukan, tampilkan pesan kesalahan
-        echo json_encode(array('error' => 'Data tidak ditemukan.'));
+        // Data NISN tidak ada, tampilkan pesan kesalahan
+        echo json_encode(array('error' => 'NISN tidak ditemukan.'));
     }
+} else {
+    // Metode bukan POST, tampilkan pesan kesalahan
+    echo json_encode(array('error' => 'Metode tidak valid.'));
 }
 
 mysqli_close($db);
