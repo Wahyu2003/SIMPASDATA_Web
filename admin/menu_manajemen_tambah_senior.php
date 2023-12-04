b6fc162c59865d348818b86af3d611f4258d3e9f
rizalmahendrag07 <71058828+RizalMahendraG07@users.noreply.github.com>
2023-12-04 13:13:40 +0700

    Update menu_manajemen_tambah_senior.php

--- admin/menu_manajemen_tambah_senior.php
+++ admin/menu_manajemen_tambah_senior.php
@@ -1,77 +1,114 @@
+<?php
+include "../main/menu.php";
+
+// Mengambil kriteria pencarian dari sisi klien (JavaScript)
+$searchTerm = isset($_GET['searchTerm']) ? $_GET['searchTerm'] : '';
+
+// Query SQL dengan kriteria pencarian
+$query = mysqli_query($db, "SELECT siswa.nisn, siswa.nama, kelas.nama AS kelas, siswa.alamat, siswa.gender, siswa.no_hp, siswa.email 
+                            FROM siswa 
+                            JOIN kelas ON siswa.kelas_id = kelas.id_kelas 
+                            WHERE role = 'junior' AND status = 'aktif' 
+                            AND (siswa.nisn LIKE '%$searchTerm%' OR siswa.nama LIKE '%$searchTerm%' OR siswa.alamat LIKE '%$searchTerm%' OR siswa.gender LIKE '%$searchTerm%' OR siswa.no_hp LIKE '%$searchTerm%' OR siswa.email LIKE '%$searchTerm%')");
+
+if (!$query) {
+    die('Error executing query: ' . mysqli_error($db));
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIM PASDATA | Tambah Akun Senior</title>
+    <title>SIM PASDATA | Manajemen Akun Junior</title>
     <link rel="stylesheet" href="../assets/css/manajemen.css">
-    
 </head>
 
 <body>
-    <?php
-    include "../main/menu.php"
-    ?>
-    <h1>Halaman Tambah Akun Senior Milik Pembina</h1>
 <div class="container">
-<a href="./menu_manajemen_akun_senior.php" class="kembali">Kembali</a>
-
-    
-    <br>
-    <div class="table">
-        <table class>
-            <thead>
-                <tr>
-                    <th>NISN</th>
-                    <th>Nama Lengkap</th>
-                    <th>Kelas</th>
-                    <th>Alamat</th>
-                    <th>Jenis Kelamin</th>
-                    <th>Email</th>
-                    <th>No. Hp</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                $query = mysqli_query($db, "SELECT siswa.nisn, siswa.nama, kelas.nama AS kelas, siswa.alamat, siswa.gender, siswa.email, siswa.no_hp FROM siswa join kelas on siswa.kelas_id = kelas.id_kelas WHERE role = 'junior' AND status = 'aktif'");
-
-                while ($a = mysqli_fetch_assoc($query)) { 
-                    $nisnSiswa = $a['nisn'];
-                    $namaSiswa = $a['nama'];
-                    $kelasSiswa = $a['kelas'];
-                    $alamatSiswa = $a['alamat'];
-                    $genderSiswa = $a['gender'];
-                    $emailSiswa = $a['email'];
-                    $noHpSiswa = $a['no_hp'];
-
-                    if($genderSiswa == 'L'){
-                        $genderSiswa = "Laki - Laki";
-                    }else{
-                        $genderSiswa = "Perempuan";
-                    }
-                    ?>
-                    <tr>
-                        <td><?= $nisnSiswa ?></td>
-                        <td><?= $namaSiswa ?></td>
-                        <td><?= $kelasSiswa ?></td>
-                        <td><?= $alamatSiswa ?></td>    
-                        <td><?= $genderSiswa ?></td>
-                        <td><?= $emailSiswa ?></td>
-                        <td><?= $noHpSiswa ?></td>
-                        <td>
-                            <a href = "?update&nisn=<?= $nisnSiswa ?>"class="jadikansenior" onclick="return confirm('Apakah kamu yakin ?')">Jadikan Senior</a>
-                        </td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
+        <?php if (isset($_GET['action']) && $_GET['action'] == 'klik') {?>
+        <div class="footer">
+            <h1 class="namaform">Data Junior</h1>
+        <?php } else { ?>
+        <h1 class="namaform">Data Junior</h1>
+        <div class="footerakun ">
+            <div class="akun">
+                <a class="tambahakun" href="./junior_manajemen_input_akun.php">Tambah Junior</a>
+            </div>
+        <?php } ?>
+        <div class="search">
+            <input type="text" id="searchInput" placeholder="Masukkan data yang ingin anda cari">
+            <img id="searchButton" src="../assets/foto/icons8-search-50.png" alt="">
+        </div>
     </div>
-</div>
-    
-   
+        <div class="table">
+            <?php if (mysqli_num_rows($query) > 0) { ?>
+                <table>
+                    <tr class="table-header">
+                        <th>NISN</th>
+                        <th>Nama Lengkap</th>
+                        <th>Kelas</th>
+                        <th>Alamat</th>
+                        <th>Jenis Kelamin</th>
+                        <th>No. Hp</th>
+                        <th>Email</th>
+                        <?php if (isset($_GET['action']) && $_GET['action'] == 'klik') {?>
+                            <th class="sembunyi">opsi</th>
+                        <?php } else { ?>
+                            <th>opsi</th>
+                        <?php
+                        }
+                        ?>
+                    </tr>
+
+                    <?php while ($a = mysqli_fetch_assoc($query)) {
+                        $nisnSiswa = $a['nisn'];
+                        $namaSiswa = $a['nama'];
+                        $kelasSiswa = $a['kelas'];
+                        $alamatSiswa = $a['alamat'];
+                        $genderSiswa = ($a['gender'] == 'L') ? "Laki - Laki" : "Perempuan";
+                        $noHpSiswa = $a['no_hp'];
+                        $emailSiswa = $a['email'];
+                    ?>
+                        <tr>
+                            <td><?= $nisnSiswa ?></td>
+                            <td><?= $namaSiswa ?></td>
+                            <td><?= $kelasSiswa ?></td>
+                            <td><?= $alamatSiswa ?></td>
+                            <td><?= $genderSiswa ?></td>
+                            <td><?= $noHpSiswa ?></td>
+                            <td><?= $emailSiswa ?></td>
+                            <?php if (isset($_GET['action']) && $_GET['action'] == 'klik') {?>
+                                <td class="sembunyi">
+                                    <div class="opsi">
+                                    <a href="?update&nisn=<?= $nisnSiswa ?>" class="jadikansenior" onclick="return confirm('Apakah kamu yakin ingin menjadikan data tersebut sebagai senior?')">Jadikan Senior</a>
+                                    <a href="./menu_manajemen_detail_senior.php?nisn=<?= $nisnSiswa ?>">Detail</a>
+                                    <a href="?delete&nisn=<?= $nisnSiswa ?>" onclick="return confirm('Apakah kamu yakin ingin menhapus data tersebut?')">Hapus</a>
+                                    </div>
+                                </td>
+                            <?php } else { ?>
+                                <td>
+                                    <div class="opsi">
+                                    <a href="?update&nisn=<?= $nisnSiswa ?>"  class="jadikansenior" onclick="return confirm('Apakah kamu yakin ingin menjadikan data tersebut sebagai senior?')">Jadikan Senior</a>
+                                    <a href="./menu_manajemen_detail_senior.php?nisn=<?= $nisnSiswa ?>"class="detail">Detail</a>
+                                    <a href="?delete&nisn=<?= $nisnSiswa ?>" class="hapus"onclick="return confirm('Apakah kamu yakin ingin menhapus data tersebut?')">Hapus</a>
+                                    </div>
+                                </td>
+                            <?php } ?>
+                        </tr>
+                    <?php } ?>
+                </table>
+            <?php } else {
+                echo "<center><p>Tidak ada data yang ditemukan</p></center>";
+            } ?>
+        </div>
+    </div>
+ 
+    <script src="../assets/js/search.js"></script>
 </body>
+
 </html>
 
 <?php
@@ -80,10 +117,23 @@ if (isset($_GET['update'])) {
 
     $update = mysqli_query($db, "UPDATE siswa SET role = 'senior', status='aktif', level='denied' WHERE siswa.nisn = '$nisn'");
     if ($update) {
-        echo '<META HTTP-EQUIV="Refresh" Content="0; URL=./menu_manajemen_akun_senior.php">';
+        echo '<META HTTP-EQUIV="Refresh" Content="0; URL=./junior_manajemen_akun.php">';
         exit;
     } else {
         echo "<script>alert('Data Gagal Diupdate!!');</script>";
+    }
+}
+if (isset($_GET['delete'])) {
+    $nisn = $_GET['nisn'];
+    
+
+    $delete = mysqli_query($db, "UPDATE siswa SET status = 'tidak' WHERE siswa.nisn = '$nisn'");
+
+    if ($delete) {
+        echo '<META HTTP-EQUIV="Refresh" Content="0; URL=./junior_manajemen_akun.php">';
+        exit;
+    } else {
+        echo "<script>alert('Data Gagal Dihapus !!');</script>";
     }
 }
 ?>
